3d12418c123e59003d2ac045304d14ac0946d510
Civitaspo <civitaspo@gmail.com>
2018-10-05 01:26:51 +0900

    Impl param.reset operator

--- src/main/scala/pro/civitaspo/digdag/plugin/param/operator/ParamResetOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/param/operator/ParamResetOperator.scala
@@ -1,12 +1,17 @@
 package pro.civitaspo.digdag.plugin.param.operator
-import io.digdag.client.config.Config
+import com.google.common.collect.ImmutableList
+import io.digdag.client.config.{Config, ConfigKey}
 import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
 
 class ParamResetOperator(operatorName: String, context: OperatorContext, systemConfig: Config, templateEngine: TemplateEngine)
   extends AbstractParamOperator(operatorName, context, systemConfig, templateEngine) {
 
+  protected val resetKey: String = params.get("_command", classOf[String])
+
   override def runTask(): TaskResult = {
-    TaskResult.empty(request)
+    val builder = TaskResult.defaultBuilder(request)
+    builder.resetStoreParams(ImmutableList.of(ConfigKey.parse(resetKey)))
+    builder.build()
   }
 
 }
